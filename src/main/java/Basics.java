42f35762491bf3d4792089f525e2403fd8f45e98
Hamzah Contreras <h.contreras9@outlook.com>
2023-04-12 00:22:00 -0500

    Building End-to-End Automation Using GET, POST, and PUT HTTP Methods

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -26,6 +26,26 @@ public class Basics {
         String placeId = js.getString("place_id");
 
         System.out.println(placeId);
+
+        //Update Place
+        String newAddress = "70 winter walk, Africa";
+
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body("{\n" +
+                        "    \"place_id\": \"" + placeId + "\",\n" +
+                        "    \"address\": \"" + newAddress + "\",\n" +
+                        "    \"key\": \"qaclick123\"\n" +
+                        "}")
+                .when().put("maps/api/place/update/json")
+                .then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+        //Get Place
+        String getPlaceResponse = given().log().all().queryParam("key", "qaclick123").queryParam("place_id", placeId)
+                .when().get("maps/api/place/get/json")
+                .then().assertThat().log().all().statusCode(200).extract().response().asString();
+        JsonPath jsonPath = new JsonPath(getPlaceResponse);
+        String actualAddress = jsonPath.getString("address");
+        System.out.println(actualAddress);
     }
 
 }
