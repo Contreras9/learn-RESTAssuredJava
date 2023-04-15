d2b20dd5c12b84d9ce82dd00aaa4e54f35d62be9
Hamzah Contreras <h.contreras9@outlook.com>
2023-04-15 04:10:16 -0500

    Iterating Over Every Element of the JSON Array and Accessing Elements in It

--- src/main/java/ComplexJsonParse.java
+++ src/main/java/ComplexJsonParse.java
@@ -15,8 +15,18 @@ public class ComplexJsonParse {
         System.out.println(totalAmount);
 
         //Print Title of the first course
-        String firstCourseTitle = jsonPath.getString("courses[0].title");
+        String firstCourseTitle = jsonPath.getString("courses[2].title");
         System.out.println(firstCourseTitle);
+
+        //Print All course titles and their respective Prices
+        for (int i = 0; i < count; i++) {
+            String courseTitles = jsonPath.getString("courses[" + i + "].title");
+            int coursePrices = jsonPath.getInt("courses[" + i + "].price");
+            System.out.println(courseTitles);
+            System.out.println(coursePrices);
+        }
+
+        
 
     }
 }
--- src/main/java/files/Payload.java
+++ src/main/java/files/Payload.java
@@ -34,14 +34,19 @@ public class Payload {
                 "      \"copies\": 6\n" +
                 "    },\n" +
                 "    {\n" +
-                "      \"title\": \"Selenium Python\",\n" +
-                "      \"price\": 50,\n" +
-                "      \"copies\": 6\n" +
+                "      \"title\": \"Cypress\",\n" +
+                "      \"price\": 40,\n" +
+                "      \"copies\": 4\n" +
                 "    },\n" +
                 "    {\n" +
-                "      \"title\": \"Selenium Python\",\n" +
-                "      \"price\": 50,\n" +
-                "      \"copies\": 6\n" +
+                "      \"title\": \"RPA\",\n" +
+                "      \"price\": 45,\n" +
+                "      \"copies\": 10\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"title\": \"Appium\",\n" +
+                "      \"price\": 36,\n" +
+                "      \"copies\": 7\n" +
                 "    }\n" +
                 "  ]\n" +
                 "}";
