bbc616f771c9bbd433270379701f82528a296432
Hamzah Contreras <h.contreras9@outlook.com>
2023-04-30 02:55:24 -0500

    How to Handle with Static JSON Payloads

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -4,11 +4,15 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class Basics {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Validate if AddPlace API is working as expected
         //Add place -> Update Place with New Address -> Get Place to validate if New Address is present in response
 
@@ -16,9 +20,11 @@ public class Basics {
         //given - All input details
         //when - Submit the api/resource, http method
         //Then - Validate the response
+        //Content of the file to String -> content of file can convert into Byte -> Byte data to String
+
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-                .body(Payload.addPlace())
+                .body(new String(Files.readAllBytes(Paths.get("/Users/hamzahcontreras/Development/Java/addPlace.json"))))
                 .when().post("maps/api/place/add/json")
                 .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
                 .header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
